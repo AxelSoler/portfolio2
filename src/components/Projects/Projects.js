77a3877c9a967f4d6082270172c5d2d35d12ad77
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-10-05 16:11:32 -0300

    All projects data

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,43 +1,31 @@
-const Projects = () => {
-  const stories = [
-    {
-      name: 'Multi-Post Stories',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
-      mobileDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
-      desktopDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
-      featured_image1: './images/design.jpg',
-      featured_image2: './images/picturePopup.png',
-      technologies: [
-        'CSS',
-        'html',
-        'bootstrap',
-        'ruby',
-      ],
-      technologies2: [
-        'html',
-        'Ruby on rails',
-        'CSS',
-        'Github',
-      ],
-      live: 'https://axelsoler.github.io/Axel-Soler-Portfolio/',
-      source: 'https://github.com/AxelSoler',
-    },
-  ];
-  return (
-    <section className="work">
-      {stories.map((story) => (
-        <div key={story.name} className="story">
-          <img className="picture" src={story.featured_image1} alt="my story" />
-          <div className="pictureDescription">
-            <h3 className="post">{story.name}</h3>
-            <p className="postDescription">{story.description}</p>
-            <button className="project" type="button">See Project</button>
-          </div>
-        </div>
-      ))}
-    </section>
-  );
-};
+import projects from './AllProjects';
+
+const Projects = () => (
+  <section className="work">
+    <h2>MY PROJECTS</h2>
+    {projects.map((project) => (
+      <div key={project.name} className="project">
+        <h3 className="post">{project.name}</h3>
+        <img className="picture" src={project.image} alt={project.name} />
+        <p className="postDescription">{project.description}</p>
+        <a href={project.live} target="_blank" rel="noopener noreferrer" className="projectLink">
+          See Live
+          {' '}
+          {project.name}
+        </a>
+        <a href={project.repository} target="_blank" rel="noopener noreferrer" className="projectLink">
+          See Repository
+          {' '}
+          {project.name}
+        </a>
+        <ul>
+          {project.technologies.map((technology) => (
+            <li key={technology}>{technology}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </section>
+);
 
 export default Projects;
