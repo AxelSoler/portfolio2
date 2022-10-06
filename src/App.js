dc6ba171088a86148d1fc0f651c359dfdbed9095
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-10-06 14:45:56 -0300

    formspree

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FormspreeProvider } from '@formspree/react';
 import SocialLinks from './components/SocialLinks/SocialLinks';
 import Navbar from './components/Navbar/Navbar';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
+import ContactForm from './components/Contact/Contact';
 
 const App = () => (
   <div className="App">
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <SocialLinks />
-    </BrowserRouter>
+    <FormspreeProvider project="2047778179661168386">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<ContactForm />} />
+        </Routes>
+        <SocialLinks />
+      </BrowserRouter>
+    </FormspreeProvider>
   </div>
 );
 
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,21 +1,54 @@
-const Contact = () => (
-  <section className="contactForm">
-    <div className="contactContainer">
-      <h2 className="contactTitle1">CONTACT ME</h2>
-      <h2 className="contactTitle2">Let’s chat!</h2>
+import React from 'react';
+import { useForm } from '@formspree/react';
+
+const ContactForm = () => {
+  const [state, handleSubmit] = useForm('contactForm');
+  if (state.succeeded) {
+    return <p>Thank you for the message!</p>;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="title">Let’s chat!</h2>
       <p className="description">
         If you have an application you are interested in developing, a
         {' '}
         feature that you need built or a project that needs coding. I’d love to help with it.
       </p>
-      <form action="https://formspree.io/f/meqnzyke" method="post">
-        <input type="text" name="usename" placeholder="Text input" required />
-        <input type="email" name="userEmail" placeholder="email@something.com" required />
-        <textarea name="userComment" placeholder="Enter text here" required />
-        <button className="submitButton" type="submit">Send Message</button>
-      </form>
-    </div>
-  </section>
-);
+      <label htmlFor="username">
+        What is your name?
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Username"
+        />
+      </label>
+      <label htmlFor="email">
+        Email Address
+        <input
+          type="email"
+          id="email"
+          name="userEmail"
+          placeholder="email@something.com"
+        />
+      </label>
+      <label htmlFor="message">
+        Send me your message
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Enter text here"
+        />
+      </label>
+      <button
+        id="submitButton"
+        type="submit"
+        disabled={state.submitting}
+      >
+        Send Message
+      </button>
+    </form>
+  );
+};
 
-export default Contact;
+export default ContactForm;
